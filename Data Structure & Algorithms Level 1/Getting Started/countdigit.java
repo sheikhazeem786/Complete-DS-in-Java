504debc28e01aa2b1359933a9766c79b6468ddaf
sheikhazeem786 <sheikhazeem141@gmail.com>
2021-09-08 01:00:12 +0530

    adding CountAllDigit

---
+++ Data Structure & Algorithms Level 1/Getting Started/countdigit.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class countdigit {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int count=0;
+        while(n!=0){
+            n=n/10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
