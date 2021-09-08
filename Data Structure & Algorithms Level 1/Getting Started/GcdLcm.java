f2fbdcd5ad1e2f259b0502559efe0e9482858167
sheikhazeem786 <sheikhazeem141@gmail.com>
2021-09-08 16:40:13 +0530

    correcting spell of file of GcdLcm

--- Data Structure & Algorithms Level 1/Getting Started/GcdLcm.java
+++ Data Structure & Algorithms Level 1/Getting Started/GcdLcm.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class GcdLcm {
+public class gcdLcm {
     public static void main(String[] args) {
         Scanner scn=new Scanner(System.in);
         int n1=scn.nextInt();
