dfb31236a915b92d9b9139b4ebf5bb82b45a94fa
sheikhazeem786 <sheikhazeem141@gmail.com>
2021-09-08 17:35:19 +0530

    adding benjaminBulb

---
+++ Data Structure & Algorithms Level 1/Getting Started/benjaminbulb.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class benjaminbulb {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        for(int i=1;i*i<=n;i++){
+            System.out.println(i*i);
+        }
+    }
+}
