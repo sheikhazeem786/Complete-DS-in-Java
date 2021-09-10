854a56d734588733343d8a343faf06584218a730
sheikhazeem786 <sheikhazeem141@gmail.com>
2021-09-10 17:21:17 +0530

    adding pattern14

---
+++ Data Structure & Algorithms Level 1/Patterns/pattern13.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class pattern13 {
+    public static void main(String[] args) {
+       Scanner scn=new Scanner(System.in);
+       int n=scn.nextInt();
+       for(int i=1;i<=10;i++){
+           System.out.println(n+" * "+i+" = "+n*i);
+       } 
+    }
+}
