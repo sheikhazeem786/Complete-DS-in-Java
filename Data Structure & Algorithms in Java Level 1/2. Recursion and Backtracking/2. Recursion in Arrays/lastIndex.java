243609dcce1df4915ddc31e88ef6df31f6741694
sheikhazeem786 <sheikhazeem141@gmail.com>
2021-11-10 22:05:22 +0530

    adding Display Last Index in Array using Recursion

--- Data Structure & Algorithms in Java Level 1/2. Recursion and Backtracking/2. Recursion in Arrays/lastIndex.java
+++ Data Structure & Algorithms in Java Level 1/2. Recursion and Backtracking/2. Recursion in Arrays/lastIndex.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class firstIndex {
+public class lastIndex {
 
     public static void main(String[] args) throws Exception {
         // write your code here
@@ -12,18 +12,25 @@ public class firstIndex {
             arr[i]=scn.nextInt();
         }
         int x=scn.nextInt();
-        System.out.println(firstIndex(arr, 0, x));
+        System.out.println(lastIndex(arr, 0, x));
     }
 
-    public static int firstIndex(int[] arr, int i, int x){
+    public static int lastIndex(int[] arr, int i, int x){
         
         if(i==arr.length) return -1;
-        if(arr[i]==x) return i;
-        else{
-            int first=firstIndex(arr, i+1, x);
-            return first;
+        int li=lastIndex(arr, i+1, x);
+        if(li==-1){
+            if(arr[i]==x){
+                return i;
+            }
+            else{
+                return -1;
+            }
         }
-
+        else{
+            return li;
+        }
+        
     }
 
 }
