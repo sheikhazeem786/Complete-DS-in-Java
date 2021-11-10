8ee1c8d021e0a311080f1ea127b4970514fe555f
sheikhazeem786 <sheikhazeem141@gmail.com>
2021-11-10 10:58:16 +0530

    adding Print Tower of Hanoi using Recursion

---
+++ Data Structure & Algorithms in Java Level 1/2. Recursion and Backtracking/1. Introduction to Recursion/printTOH.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class printTOH {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn= new Scanner(System.in);
+        int n=scn.nextInt();
+        int src=scn.nextInt();
+        int dest=scn.nextInt();
+        int help=scn.nextInt();
+        toh(n,src,dest,help);
+
+    }
+
+    public static void toh(int n, int src, int dest, int help){
+        if(n==0) return;
+        toh(n-1, src, help, dest);
+        System.out.println(n + "[" + src + "->" + dest + "]");
+        toh(n-1, help, dest, src);
+
+    }
+
+}
