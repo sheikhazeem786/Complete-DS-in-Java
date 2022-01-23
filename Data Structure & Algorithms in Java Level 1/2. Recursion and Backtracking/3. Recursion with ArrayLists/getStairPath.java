7c6edc8221d9877b5be775b4522276417efa0612
sheikhazeem786 <sheikhazeem141@gmail.com>
2022-01-23 16:09:38 +0530

    adding printStairPaths in Recursion on the way up

--- Data Structure & Algorithms in Java Level 1/2. Recursion and Backtracking/3. Recursion with ArrayLists/getStairPath.java
+++ Data Structure & Algorithms in Java Level 1/2. Recursion and Backtracking/3. Recursion with ArrayLists/getStairPath.java
@@ -11,13 +11,13 @@ public class getStairPath {
     }
 
     public static ArrayList<String> getStairPaths(int n) {
-        if(n==0){
-            ArrayList<String> bres= new ArrayList<>();
-            bres.add("");
-            return bres;
-        }else if(n<0){
-            ArrayList<String> bres= new ArrayList<>();
-            return bres;
+        if(n<0){
+            ArrayList<String> bsp= new ArrayList<>();
+            return bsp;
+        }else if(n==0){
+            ArrayList<String> bsp= new ArrayList<>();
+            bsp.add("");
+            return bsp;
         }
 
         ArrayList<String> pathsfromnm1=getStairPaths(n-1);
@@ -27,16 +27,13 @@ public class getStairPath {
         ArrayList<String> pathsfromn=new ArrayList<>();
 
         for(String pathfromnm1: pathsfromnm1){
-            String pathfromn="1"+pathfromnm1;
-            pathsfromn.add(pathfromn);
+            pathsfromn.add("1"+pathfromnm1);
         }
-        for(String pathfromnm1: pathsfromnm2){
-            String pathfromn="2"+pathfromnm1;
-            pathsfromn.add(pathfromn);
+        for(String pathfromnm2: pathsfromnm2){
+            pathsfromn.add("2"+pathfromnm2);
         }
-        for(String pathfromnm1: pathsfromnm3){
-            String pathfromn="3"+pathfromnm1;
-            pathsfromn.add(pathfromn);
+        for(String pathfromnm3: pathsfromnm3){
+            pathsfromn.add("3"+pathfromnm3);
         }
 
 
@@ -44,3 +41,46 @@ public class getStairPath {
     }
 
 }
+
+// import java.io.*;
+// import java.util.*;
+
+// public class getStairPath {
+
+//     public static void main(String[] args) throws Exception {
+
+//         Scanner scn=new Scanner(System.in);
+//         int n=scn.nextInt();
+//         ArrayList<String> gsp=getStairPaths(n);
+//         System.out.println(gsp);
+//     }
+
+//     public static ArrayList<String> getStairPaths(int n) {
+        
+//         if(n<0){
+//             ArrayList<String> bsp=new ArrayList<>();
+//             return bsp;
+//         }else if(n==0){
+//             ArrayList<String> bsp=new ArrayList<>();
+//             bsp.add("");
+//             return bsp;
+//         }
+
+//         ArrayList<String> gspfm1=getStairPaths(n-1);
+//         ArrayList<String> gspfm2=getStairPaths(n-2);
+//         ArrayList<String> gspfm3=getStairPaths(n-3);
+//         ArrayList<String> gspfn=new ArrayList<>();
+
+//         for(String getstairpath:gspfm1){
+//             gspfn.add("1"+getstairpath);
+//         }
+//         for(String getstairpath:gspfm2){
+//             gspfn.add("2"+getstairpath);
+//         }
+//         for(String getstairpath:gspfm3){
+//             gspfn.add("3"+getstairpath);
+//         }
+//         return gspfn;
+//     }
+
+// }
