56f8c767e5eb71f88e71ac30be2f6a9ccb32ebfd
sheikhazeem786 <sheikhazeem141@gmail.com>
2022-02-03 00:36:03 +0530

    adding printPermutation in Recursion on the way up

---
+++ Data Structure & Algorithms in Java Level 1/2. Recursion and Backtracking/4. Recursion on the way up/printPermutation.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class printPermutation {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner scn=new Scanner(System.in);
+        String str=scn.nextLine();
+        printPermutations(str, "");
+    }
+
+    public static void printPermutations(String str, String asf) {
+        
+        if(str.length()==0){
+            System.out.println(asf);
+            return;
+        }
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            String left=str.substring(0, i);
+            String right=str.substring(i+1);
+            printPermutations(left+right, asf+ch);
+        }
+    }
+
+}
